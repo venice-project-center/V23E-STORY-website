bc0e75ff364eca6aa6a142db4f8f0fc1ae39a125
Alireza Dehghan <alirezaxdehghan@gmail.com>
2024-04-23 17:07:31 +0200

    bug fixing

--- src/app/components/ProjectContent/editingCode/CommitProject.ts
+++ src/app/components/ProjectContent/editingCode/CommitProject.ts
@@ -5,14 +5,9 @@ export async function CommitProject(project:FullProject):Promise<string> {
     console.log(project.dataurls)
     let createResponse = await fetch(window.location.origin+"/api/projects",{
         method:"POST",
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body:JSON.stringify(project)
+        body:JSON.stringify(project),
     });
-    console.log(createResponse)
     let projectData:FullProject = await createResponse.json();
-    console.log(createResponse)
     console.log(projectData);
     return projectData.id
 }
